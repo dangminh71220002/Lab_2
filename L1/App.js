0cb9c60fc811c0a7aa1859f76088e545f69a691c
Nguyen Dang Minh <83014500+dangminh71220002@users.noreply.github.com>
2022-06-20 10:59:11 +0700

    Flex Screen
    Phone screen ratio

--- L1/App.js
+++ L1/App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TouchableOpacity } from 'react-native';
+import { Button } from 'react-native-web';
+import React, { useState } from "react";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+      </View>
+      <View style={styles.midBody}>
+      </View>
     </View>
   );
 }
@@ -13,8 +17,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  header:{
+    flex: 10,
+    backgroundColor:'blue',
+    color:"blue",
+    // alignItems: 'center',
+     justifyContent: 'center',
+  },
+  midBody:{
+    flex:90,
+    backgroundColor:'red',
   },
 });
